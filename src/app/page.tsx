efbdc5e526a378c0691d914addf821383592bc36
Exion <harashnabin2002@gmail.com>
2023-08-10 12:43:10 +0530

    Added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
     <div className='text-[#fff]'>
       
       
-      <div className='flex flex-col bg-hero bg-cover bg-center min-h-screen px-[100px]'>
+      <div className='flex flex-col bg-hero bg-cover bg-center min-h-screen'>
         <div className='p-20'>
           <Image src="/logo.jpeg" alt='Logo' width={200} height={200} />
         </div>
-        <div className='flex justify-center text-[150px] font-cookie italic '>Aghanya</div>
+        <div className='flex justify-center text-[250px] font-cookie italic '>Aghanya</div>
         {/* <Image src="/aghanyahero.png" alt='aghanya' height={200} width={700} /> */}
-        <div className='flex justify-end mr-40 text-[40px] font-roberto'>Smell-of-Earth Series</div>
+        <div className='flex justify-end mr-40 text-[50px] font-roberto'>Smell-of-Earth Series</div>
       </div>
 
 
@@ -27,19 +27,27 @@ export default function Home() {
         <div className='pl-20 text-[80px] -translate-y-10'>PRODUCTS</div>
         <div className='p-20 flex flex-row gap-16 flex-wrap'>
           <div className='-translate-y-6'>
+            <Link href="#Vermicompost" scroll={false}>
               <Image src="/VC.png" alt='Product1' width={350} height={550} />
+            </Link>
               <div className='flex justify-center -mt-6 text-3xl'>Vermicompost</div>
           </div>
           <div>
-              <Image src="/image2.png" alt='Product2' width={330} height={550} />
+              <Link href="#Potting">
+                <Image src="/image2.png" alt='Product2' width={330} height={550} />
+              </Link>
               <div className='flex justify-center text-3xl mt-6'>Potting Mix</div>
           </div>
           <div>
+            <Link href="#Coco">
               <Image src="/image3.png" alt='Product3' width={330} height={550} />
+            </Link>
               <div className='flex justify-center text-3xl mt-8'>Coco Peat</div>
           </div>
           <div>
-              <Image src="/diya.png" alt='Product4' width={300} height={550} />
+              <Link href="#diya">
+                <Image src="/diya.png" alt='Product4' width={300} height={550} />
+              </Link>
               <div className='flex justify-center text-3xl mt-40'>Panchagavya Diya</div>
           </div>
         </div>
@@ -48,10 +56,18 @@ export default function Home() {
 
 
       <div className='Products'>
-        <Vermicompost />
-        <PottingMix />
-        <CocoPeat />
-        <Diya />
+        <section id='Vermicompost'>
+          <Vermicompost />
+        </section>
+        <section id='Potting'>
+          <PottingMix />
+        </section>
+        <section id='Coco'>
+          <CocoPeat />
+        </section>
+        <section id='diya'>
+          <Diya />
+        </section>
       </div>
       <Footer />
     </div>
